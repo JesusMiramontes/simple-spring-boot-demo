5ff0d625faebc9a19a9d64c76f17f3104ef4fbb3
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-10-04 17:30:52 -0500

    Moved logic to a service layer

--- src/main/java/com/miramontes/demo/student/StudentController.java
+++ src/main/java/com/miramontes/demo/student/StudentController.java
@@ -1,20 +1,25 @@
 package com.miramontes.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/student")
 public class StudentController {
 
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping
     public List<Student> getStudent(){
-        return List.of(
-                new Student(1L,"Jesus", "jesus@mail.com", LocalDate.of(1990,01,01), 31)
-        );
+        return studentService.getStudents();
     }
 }
