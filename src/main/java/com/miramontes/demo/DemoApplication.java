a4864164aacec625c38917e587c42bc16a95b7d8
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-10-04 16:28:52 -0500

    Return simple list of mock students

--- src/main/java/com/miramontes/demo/DemoApplication.java
+++ src/main/java/com/miramontes/demo/DemoApplication.java
@@ -1,13 +1,26 @@
 package com.miramontes.demo;
 
+import com.miramontes.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping
+	public List<Student> hell(){
+		return List.of(
+				new Student(1L,"Jesus", "jesus@mail.com", LocalDate.of(1990,01,01), 31)
+		);
+	}
 }
