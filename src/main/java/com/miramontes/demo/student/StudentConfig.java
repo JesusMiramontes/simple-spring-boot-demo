16422e24b6d06c95ef39cba064a6752e6d681b2e
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-10-05 00:28:46 -0500

    Seed students into db

---
+++ src/main/java/com/miramontes/demo/student/StudentConfig.java
@@ -0,0 +1,22 @@
+package com.miramontes.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student jesus = new Student("Jesus", "jesus@mail.com", LocalDate.of(1990,01,01), 31);
+            Student alex = new Student("Alex", "alex@mail.com", LocalDate.of(2001,01,01), 20);
+
+            repository.saveAll(List.of(jesus, alex));
+        };
+    }
+}
