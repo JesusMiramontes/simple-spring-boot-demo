dc8cc4d84d729aa88f4a3e25a6149c76a016a027
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-10-05 00:23:23 -0500

    Created StudentService to perform db operations

--- src/main/java/com/miramontes/demo/student/StudentService.java
+++ src/main/java/com/miramontes/demo/student/StudentService.java
@@ -1,15 +1,21 @@
 package com.miramontes.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(1L,"Jesus", "jesus@mail.com", LocalDate.of(1990,01,01), 31)
-        );
+        return studentRepository.findAll();
     }
 }
